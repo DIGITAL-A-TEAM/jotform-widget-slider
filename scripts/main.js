7582737c1e4910660b77fd7f0e40fae7d2f207d3
den2702 <92326139+den2702@users.noreply.github.com>
2022-02-23 16:47:01 +0100

    format option add

--- scripts/main.js
+++ scripts/main.js
@@ -1,23 +1,27 @@
 
-function formatVal(number) {
-    number = '' + parseFloat(number);
-    if (number.length > 3) {
-        let mod = number.length % 3;
-        let output = (mod > 0 ? (number.substring(0,mod)) : '');
-        for (i=0 ; i < Math.floor(number.length / 3); i++) {
-            if ((mod == 0) && (i == 0))
-                output += number.substring(mod+ 3 * i, mod + 3 * i + 3);
-            else
-                output+= '.' + number.substring(mod + 3 * i, mod + 3 * i + 3);
+function formatVal(number, format) {
+    if (format == 'float') {
+        number = '' + parseFloat(number);
+        if (number.length > 3) {
+            let mod = number.length % 3;
+            let output = (mod > 0 ? (number.substring(0,mod)) : '');
+            for (i=0 ; i < Math.floor(number.length / 3); i++) {
+                if ((mod == 0) && (i == 0))
+                    output += number.substring(mod+ 3 * i, mod + 3 * i + 3);
+                else
+                    output+= '.' + number.substring(mod + 3 * i, mod + 3 * i + 3);
+            }
+            return (output);
         }
-        return (output);
+        else 
+            return number;
     }
     else 
         return number;
 }
 
-function showVal(value, valueUnit) {
-    document.getElementById("curVal").innerHTML = formatVal(value) + ' ' + valueUnit;
+function showVal(value, valueUnit, format) {
+    document.getElementById("curVal").innerHTML = formatVal(value, format) + ' ' + valueUnit;
 }
 
 JFCustomWidget.subscribe("ready", function () {
@@ -30,15 +34,15 @@ JFCustomWidget.subscribe("ready", function () {
     else {
         valueUnit = jotformSettings.unit;
     }
-    document.getElementById("curVal").innerHTML = formatVal(jotformSettings.defaultValue.trim()) + ' ' + valueUnit;
+    document.getElementById("curVal").innerHTML = formatVal(jotformSettings.defaultValue.trim(), jotformSettings.format) + ' ' + valueUnit;
     // console.log(label);
-    document.getElementById('dat-slider').addEventListener('change', () => { showVal(document.getElementById('dat-slider').value, valueUnit) });
-    document.getElementById('dat-slider').addEventListener('input', () => { showVal(document.getElementById('dat-slider').value, valueUnit) });
+    document.getElementById('dat-slider').addEventListener('change', () => { showVal(document.getElementById('dat-slider').value, valueUnit, jotformSettings.format) });
+    document.getElementById('dat-slider').addEventListener('input', () => { showVal(document.getElementById('dat-slider').value, valueUnit, jotformSettings.format) });
 
     label != '' ? document.getElementById('label').innerHTML = label : document.getElementById('label').innerHTML = '';
     jotformSettings.minValue != '' ? document.getElementById('dat-slider').min = parseFloat(jotformSettings.minValue.trim()) : document.getElementById('dat-slider').min = 0;
     
-    if (jotformSettings.currentYear == 'yes') {
+    if (jotformSettings.format == 'current year') {
         document.getElementById('dat-slider').max = new Date().getFullYear(); 
     }
     else {
@@ -55,8 +59,8 @@ JFCustomWidget.subscribe("ready", function () {
         style.innerHTML = ".range::-webkit-slider-thumb { background: " + jotformSettings.colorThumb + " !important; }";
     }
 
-    document.getElementById('minValue').innerHTML = formatVal(jotformSettings.minValue.trim()) + ' ' + valueUnit;
-    document.getElementById('maxValue').innerHTML = formatVal(jotformSettings.maxValue.trim()) + ' ' + valueUnit;
+    document.getElementById('minValue').innerHTML = formatVal(jotformSettings.minValue.trim(), jotformSettings.format) + ' ' + valueUnit;
+    document.getElementById('maxValue').innerHTML = formatVal(jotformSettings.maxValue.trim(), jotformSettings.format) + ' ' + valueUnit;
 
     JFCustomWidget.subscribe("submit", function () {
 
